af8d7f752a9fafa3e306746c90b13963c96f4c85
sky107 <siddharthsk1234@gmail.com>
2022-04-28 06:36:07 +0530

    added firebase

--- server.js
+++ server.js
@@ -1,16 +1,75 @@
-const app=require('express')();
+const app = require("express")();
+const bodyParser = require("body-parser");
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
 
+var admin = require("firebase-admin");
 
+var serviceAccount = require("./motioncloudwatch-firebase-adminsdk-5m5xw-b872807fb5.json");
+const { json } = require("body-parser");
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  
+});
 
-app.post('/send-notification',(req,res,next)=>{
+const db = getFirestore();
+var jsonParser = bodyParser.json();
 
-    console.log(req.body);
+app.post("/send-notification", jsonParser, async (req, res, next) => {
+  const { email } = req.body;
+  console.log(email);
 
-    res.send({
-        message:"RECEIVED"
-    })
+  const snapshot = await db
+    .collection("AppUsers")
+    .where("email", "==", email)
+    .get();
 
-})
+  if (snapshot.empty) {
+    console.log("No matching documents.");
+    return;
+  }
+  snapshot.forEach((doc) => {
+    const { fcmToken } = doc.data();
 
-app.listen(process.env.PORT || 3000,()=>console.log("OK"))
+    var options = {
+      priority: "normal",
+      timeToLive: 60 * 60,
+    };
+
+    var payload = {
+      notification: {
+        title: "Account Deposit",
+        body: "A deposit to your savings account has just cleared.",
+      },
+      data: {
+        account: "Savings",
+        balance: "$3020.25",
+      },
+    };
+    console.log("FCM",fcmToken)
+    admin
+      .messaging()
+      .sendToDevice(fcmToken, payload, options)
+      .then(function (response) {
+        console.log("Successfully sent message:", response.results);
+      })
+      .catch(function (error) {
+        console.log("Error sending message:", error);
+      });
+
+    res.json({
+      message: "SUCCESS",
+    });
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("OK"));
